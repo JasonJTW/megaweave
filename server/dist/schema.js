493cb08ef83ca15755600366c9b707ae48687d0b
jgh0604043 <way6107715@gmail.com>
2025-07-12 13:01:34 +0800

    fix(CORS): Add cors env

--- server/dist/schema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoles = void 0;
-exports.userRoles = ["admin", "user"];
-//# sourceMappingURL=schema.js.map

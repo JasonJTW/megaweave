86fc791363530db01d59334c1d5ff5679c4e0d95
jgh0604043 <way6107715@gmail.com>
2025-08-07 00:49:24 +0800

    fix(FE_about): Fix some bug

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef } from "react";
-import { TeamMember, teamMembers } from "../teamMembers";
+import { TeamMember, teamMembers } from "@/app/teamMembers";
 import { useRouter } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 

97b0ee8819ecd682175474d90de023dba30d8fc4
jgh0604043 <way6107715@gmail.com>
2025-09-13 15:24:04 +0800

    fix(AdSense): Fix again

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${publisherId}`}
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <body

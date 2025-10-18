f5622ca33754e600a5bb50546291619eb5d414c5
jgh0604043 <way6107715@gmail.com>
2025-10-18 19:09:41 +0800

    fix(FE_PostPage): Fix some bug

--- client/app/Card/page.tsx
+++ client/app/Card/page.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import CardV2 from "../components/PostCard/CardV2";
 import { useState, useCallback, useEffect } from "react";
 import { usePost } from "../contexts/PostContext";
-import { PostsResponse, Post, Condition, Category } from "../types/schema";
+import { PostsResponse, Post } from "../types/schema";
 import { useRouter } from "next/navigation";
-import Footer from "../components/Footer";
 const CardPage = () => {
   const router = useRouter();
   const hostName = process.env.NEXT_PUBLIC_HOSTNAME;
   const [posts, setPosts] = useState<Post[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage] = useState(1);
   const { categories, conditions } = usePost();
 
   const fetchPosts = useCallback(async () => {
@@ -29,7 +28,7 @@ const CardPage = () => {
     } catch (error) {
       console.error("Internal server error:", error);
     }
-  }, [hostName]);
+  }, [hostName, currentPage]);
   useEffect(() => {
     fetchPosts();
   }, [fetchPosts]);

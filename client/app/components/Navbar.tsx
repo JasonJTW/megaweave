9b7c2ae2e18b87884e18ee83db7dde454435a36e
jgh0604043 <way6107715@gmail.com>
2025-10-25 16:37:28 +0800

    fix(share): Fix share bug

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -139,7 +139,7 @@ const Navbar = () => {
         }`}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center h-24">
+          <div className="flex justify-between items-center h-20">
             {/* Logo */}
             <Link href="/" className="flex-shrink-0">
               <div className="flex items-center space-x-3 ">

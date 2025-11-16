7910952f77c5fca06ed10e808df77d698c057d4b
jgh0604043 <way6107715@gmail.com>
2025-11-16 23:24:23 +0800

    fix(redis): Log redisurl

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -28,6 +28,7 @@ const redisClient = createClient({
 
 redisClient.on("error", (err) => {
   console.error("Redis Client Error", err.message);
+  console.log("Redis URL: ", process.env.REDIS_URL);
   isConnecting = false;
 });
 

1adac412047b4997e48a4e173aafacb690ec93c3
jgh0604043 <way6107715@gmail.com>
2026-04-11 19:57:42 +0800

    fix(FE): Implement auto hide&show in <Footer>

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import Link from "next/link";
 import { FC } from "react";
 import WeavingIcon from "./icons/WeavingIcon";
+import { useNavbar } from "../contexts/NavBarContext";
+import { cn } from "@/lib/utils";
 
 const Footer: FC = () => {
+  const { isNavbarVisible } = useNavbar();
+
   return (
-    <footer className=" overflow-hidden sm:sticky sm:z-40 sm:bottom-0 bg-[#f4f5f3] text-[#14321f] font-ddin max-w-full">
+    <footer
+      className={cn(
+        "overflow-hidden sm:sticky sm:z-40 sm:bottom-0 bg-[#f4f5f3] text-[#14321f] font-ddin max-w-full transition-transform duration-300 ease-in-out",
+        isNavbarVisible ? "translate-y-0" : "translate-y-full"
+      )}
+    >
       {/* 頂端細線（幾乎不可見） */}
 
       {/* 主要內容區（置於 logo 之上） */}

1bd0d0a4eeefa7a5812255916167c1f7401d8d74
jgh0604043 <way6107715@gmail.com>
2026-04-13 23:50:32 +0800

    fix(FE): Fix google maps address language bug

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -177,7 +177,7 @@ export default function RootLayout({
             <Script
               id="google-maps"
               strategy="afterInteractive"
-              src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&loading=async`}
+              src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&language=zh-TW&region=TW&loading=async`}
             />
             <GoogleOAuthProvider
               clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}

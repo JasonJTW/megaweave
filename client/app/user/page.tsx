c85106c2de61076099bce597afd4c2746c36b3a4
jgh0604043 <way6107715@gmail.com>
2026-05-09 20:40:35 +0800

    fix(contributor badge): Fix get error

--- client/app/user/page.tsx
+++ client/app/user/page.tsx
@@ -1092,6 +1092,7 @@ const UserPage = () => {
                           alt="contributor-badge"
                           width={20}
                           height={20}
+                          unoptimized
                         />
                         <p className="text-md text-megaweave-blue-light font-ddin font-bold tracking-widest">
                           Welcome, dear contributor!
